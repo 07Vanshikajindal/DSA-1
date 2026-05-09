8f78558a6f9c6921a2a2191b4a9fa71361a3d6b8
07Vanshikajindal <157563909+07Vanshikajindal@users.noreply.github.com>
2026-05-09 06:36:29 +0530

    Time: 84 ms (5.11%), Space: 44.7 MB (85.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -3,27 +3,29 @@ import java.util.*;
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        Stack<Integer> st = new Stack<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Traverse nums2 from right to left
-        for (int i = nums2.length - 1; i >= 0; i--) {
-
-            while (!st.isEmpty() && st.peek() <= nums2[i]) {
-                st.pop();
-            }
-
-            int nge = st.isEmpty() ? -1 : st.peek();
-            map.put(nums2[i], nge);
-
-            st.push(nums2[i]);
-        }
-
-        // Build result for nums1
         int[] res = new int[nums1.length];
 
         for (int i = 0; i < nums1.length; i++) {
-            res[i] = map.get(nums1[i]);
+            
+            int current = nums1[i];
+            int nextGreater = -1;
+            boolean found = false;
+
+            // find position of current in nums2
+            for (int j = 0; j < nums2.length; j++) {
+                
+                if (nums2[j] == current) {
+                    found = true;
+                }
+
+                // after finding element, check right side
+                if (found && nums2[j] > current) {
+                    nextGreater = nums2[j];
+                    break;
+                }
+            }
+
+            res[i] = nextGreater;
         }
 
         return res;
