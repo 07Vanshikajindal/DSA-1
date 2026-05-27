3ac921998f378751cdb4e61add951b9aba2043bc
07Vanshikajindal <157563909+07Vanshikajindal@users.noreply.github.com>
2026-05-27 20:00:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (98.72%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -1,13 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) {
+        ListNode fast = head ;
+        while (fast !=null && fast.next!=null){
             slow = slow.next;
-            fast = fast.next.next;
+            fast= fast.next.next;
         }
-
         return slow;
     }
+    //  public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter number of nodes: ");
+    //     int n = sc.nextInt();
+
+    //     if (n == 0) {
+    //         System.out.println("List is empty");
+    //         return;
+    //     }
+
+    //     // Create linked list
+    //     System.out.println("Enter elements:");
+    //     ListNode head = new ListNode(sc.nextInt());
+    //     ListNode current = head;
+
+    //     for (int i = 1; i < n; i++) {
+    //         current.next = new ListNode(sc.nextInt());
+    //         current = current.next;
+    //     }
+
+    //     // Find middle
+    //     ListNode mid = middleNode(head);
+    //  System.out.println("Middle node and onward:");
+    //     while (mid != null) {
+    //         System.out.print(mid.val + " ");
+    //         mid = mid.next;
+    //     }
+    // }
 }
