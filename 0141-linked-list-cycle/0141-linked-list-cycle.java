369796bec58bad8c5289eed0470fa7484acc7353
07Vanshikajindal <157563909+07Vanshikajindal@users.noreply.github.com>
2026-05-27 20:18:13 +0530

    Time: 0 ms (100%), Space: 46.9 MB (32.12%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -25,4 +25,42 @@ public class Solution {
         }
         return false;
     }
+    //  public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.print("Enter number of nodes: ");
+    //     int n = sc.nextInt();
+
+    //     if (n == 0) {
+    //         System.out.println("List is empty");
+    //         return;
+    //     }
+
+    //     // Create list
+    //     System.out.println("Enter elements:");
+    //     ListNode head = new ListNode(sc.nextInt());
+    //     ListNode current = head;
+
+    //     List<ListNode> nodes = new ArrayList<>();
+    //     nodes.add(head);
+
+    //     for (int i = 1; i < n; i++) {
+    //         current.next = new ListNode(sc.nextInt());
+    //         current = current.next;
+    //         nodes.add(current);
+    //     }
+
+    //     // Create cycle manually
+    //     System.out.print("Enter position to connect tail (-1 for no cycle): ");
+    //     int pos = sc.nextInt();
+
+    //     if (pos != -1) {
+    //         current.next = nodes.get(pos);
+    //     }
+
+    //     // Check cycle
+    //     boolean result = hasCycle(head);
+
+    //     System.out.println("Cycle present? " + result);
+    // }
 }
