d6456c4a74fce9329273940faa3abf1029caf91a
07Vanshikajindal <157563909+07Vanshikajindal@users.noreply.github.com>
2026-05-29 12:28:29 +0530

    Time: 4 ms (75.77%), Space: 45.2 MB (61.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,33 +1,42 @@
-import java.util.*;
-
+import  java.util.*;
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        int[] res = new int[nums1.length];
-
-        for (int i = 0; i < nums1.length; i++) {
-            
-            int current = nums1[i];
-            int nextGreater = -1;
-            boolean found = false;
-
-            // find position of current in nums2
-            for (int j = 0; j < nums2.length; j++) {
-                
-                if (nums2[j] == current) {
-                    found = true;
-                }
-
-                // after finding element, check right side
-                if (found && nums2[j] > current) {
-                    nextGreater = nums2[j];
-                    break;
-                }
+    public static int [] nextGreaterElement(int [] nums1, int[]nums2){
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack <Integer> stack = new Stack <>();
+        for ( int num : nums2){
+            while (!stack.isEmpty() && stack.peek() < num){
+                map.put(stack.pop() , num);
             }
-
-            res[i] = nextGreater;
+            stack.push(num);
         }
-
+        while (!stack.isEmpty()){
+            map.put(stack.pop(), -1);
+        }
+        int [] res = new int [nums1.length];
+        for ( int i =0;i<nums1.length;i++){
+            res[i] = map.get(nums1[i]);
+        }
         return res;
     }
+    // public static void main ( String [] args){
+    //     int n = sc.nextInt();
+    //     int [] nums1 = new int [n];
+    //     for( int i =0;i<n;i++){
+    //         nums1[i] = sc.nextInt();
+    //     }
+    //     int m = sc.nextInt();
+    //     int [] nums2 = new int [m];
+        
+    //     for ( int i =0;i<m;i++){
+    //         muns2 [i]= sc.nextInt();
+            
+    //     }
+    //     Solution ob = new Solution; 
+    //     int [] res = ob.nextGreaterElement(nums1, nums2);
+        
+    //     for ( int val : result){
+    //         System.out.print(val+ " ");
+    //     }
+    //     sc.close();
+    // }
 }
